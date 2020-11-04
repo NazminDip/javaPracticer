47d8e202c58fceec4dfccff0be868d838307dc32
Nazmin <48346432+NazminDip@users.noreply.github.com>
2020-11-04 12:36:33 +0600

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,27 @@
+
+package WhileOrdoWhileLoop;
+
+
+public class Dowhile {
+    public static void main(String[] args) {
+        int i=4;
+//        do{
+//             System.out.println(i+"Java");
+//              i++;
+//        }while(i<=5);
+//    }
+
+
+//while er por ja thake tai condition
+//for loop ba while ba do while j kono akta formulai loop kora zai
+//do while e agge do mane 1 ber print korbe condition na mile o
+
+     do{
+             System.out.println(i+"Java");
+              i++;
+        }while(i<=4);
+     //condition na mille o akbar print korbe
+    }
+    
+    
+}
