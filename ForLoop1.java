4293d546b849b0963cb2425c4457df5c7953ec0c
Nazmin <48346432+NazminDip@users.noreply.github.com>
2020-11-04 12:35:08 +0600

    Add files via upload

---
+++ ForLoop1.java
@@ -0,0 +1,14 @@
+
+package FoorLoop;
+
+public class ForLoop1 {
+    // loop mane kono akta kaaz bar bar kora
+    //ata for/while/do while diye kora zai
+   
+    public static void main(String[] args) {
+         for(int i=1; i<=50; i++){
+            
+            System.out.println(i+""+"i am first loop");
+            
+}}
+}
---
+++ OnetoTenPrint.java
@@ -0,0 +1,15 @@
+
+package FoorLoop;
+
+public class OnetoTenPrint {
+    public static void main(String[] args) {
+        
+        //i mane start koto theke initial part
+        //ar por condition koto khon cholbe
+        //er por increment ba decrement
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+  
+}
