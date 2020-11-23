f48214061b0b0b6eb02cb96fc96813bf9d958578
Nazmin <48346432+NazminDip@users.noreply.github.com>
2020-11-23 20:04:38 +0600

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+
+package FoorLoop;
+
+public class Multiplication {
+    // loop mane kono akta kaaz bar bar kora
+    //ata for/while/do while diye kora zai
+   
+    public static void main(String[] args) {
+       for(int i=1; i<=10; i++){
+            for(int j=1; j<=10; j++){
+                System.out.println(""+i+ "X" +j+"="+(i*j));
+            }
+                System.out.println("");
+            }
+    }
+}
